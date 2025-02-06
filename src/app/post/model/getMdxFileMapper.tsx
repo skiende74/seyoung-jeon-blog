12225819641890f2117e9132bd08b3b5e9d4889c
skiende74 <skiende74@gmail.com>
2025-02-06 15:40:40 +0900

    feat: frontmatter type정의

--- src/app/post/model/getMdxFileMapper.tsx
+++ src/app/post/model/getMdxFileMapper.tsx
@@ -4,23 +4,42 @@ import path from 'path'
 import { cwd } from 'process'
 import type { FC } from 'react'
 
+interface Frontmatter {
+  slug: string
+  title: string
+  date: string
+}
+interface MDXFile {
+  frontmatter: Frontmatter
+  default: FC
+}
+
 const MDX_PATH = path.join(cwd(), 'static/post')
 const filenames = readdirSync(MDX_PATH)
 
-const fileObj: Record<string, { frontmatter: {}; default: FC }> = {}
+const MdxFileObj: Record<string, MDXFile> = {}
 let isCalled = false
 
+/* {[slug]: MDXFile} 인 객체 fileObj를 반환하는 함수입니다. 
+ 캐싱하여 여러번 호출시에도 이전에 만들어둔 fileObj를 반환합니다.
+*/
+
 export const getMDXFileMapper = async () => {
-  if (isCalled) return fileObj
+  if (isCalled) return MdxFileObj
   isCalled = true
 
-  const filesPromise = filenames.map((filename) => {
-    return import('../../../../static/post/' + filename)
-  })
+  await makeMDXFileObj(filesPromise)
+
+  return MdxFileObj
+}
+
+async function makeMDXFileObj(filesPromise: Promise<MDXFile>[]) {
   const files = await Promise.all(filesPromise)
   files.forEach((file) => {
-    fileObj[file.frontmatter.slug as string] = file
+    MdxFileObj[file.frontmatter.slug] = file
   })
-
-  return fileObj
 }
+
+const filesPromise = filenames.map(
+  (filename) => import('../../../../static/post/' + filename)
+)
