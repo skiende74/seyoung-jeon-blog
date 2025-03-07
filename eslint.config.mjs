006dcade72aaef3d568f44055ad88d6842c1a2da
skiende74 <skiende74@gmail.com>
2025-03-08 03:50:18 +0900

    chore: no-unused-var를 _로 시작하는 변수는 허용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,19 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   eslintPluginPrettier,
+  {
+    // rules 객체 안에 no-unused-vars를 설정
+    rules: {
+      // @typescript-eslint/no-unused-vars에 대해 args/varsIgnorePattern 사용
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
 ]
 
 export default eslintConfig
