5b3b64ed3598b1c895f3b6fd7c3b9f87159274ec
skiende74 <skiende74@gmail.com>
2025-03-10 22:10:46 +0900

    feat: 포스팅에 편집 버튼 추가

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { getMDXFileMapper } from '../model/getMdxFileMapper'
 import { getMDXBySlug } from '../model/MDXFileService'
 import GiscusComp from './Giscus'
+import Link from 'next/link'
 
 async function page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
@@ -14,7 +15,10 @@ async function page({ params }: { params: Promise<{ slug: string }> }) {
   return (
     <article className="mt-5 mb-20 rounded-xl bg-neutral-700 px-15 py-10">
       <h1>{title}</h1>
-      <div className="text-right">{date}</div>
+      <div className="flex justify-end gap-x-4">
+        <Link href={`/post/edit/${slug}`}>편집</Link>
+        <div className="text-right">{date}</div>
+      </div>
       <MDXPost />
       <GiscusComp />
     </article>
